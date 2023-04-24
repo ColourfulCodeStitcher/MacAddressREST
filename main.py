590900b33542333334c4c1d145d9883d1e59dd55
indeg <indegcrane@tutanota.com>
2023-04-24 22:08:23 +0100

    main.py: run function, making API request

--- main.py
+++ main.py
@@ -8,21 +8,24 @@ parser.add_argument("-a", "--address", required=True, nargs=1)
 args = parser.parse_args()
 
 
-def input_checker(arguments):
+def input_checker(address):
     # make condition to check if input is a valid MAC address
     # return True if valid, False if invalid
     pass
 
 
 def query_api(arguments):
-    # make query to the api here
+    response = requests.get(f"https://api.macaddress.io/v1?apiKey={arguments.key}&output=json&search={arguments.address}")
+    # TODO: format response from JSON to useful format - just a string?
     pass
 
 
 def run(arguments):
-    # call the other functions in here based on some conditions
-    # still need to decide on a useful format for the output
-    pass
+    valid = inputchecker(arguments.address)
+    if valid:
+        query_api(arguments)
+    else:
+        print("Invalid MAC address. Please try again")
 
 
 run(args)
