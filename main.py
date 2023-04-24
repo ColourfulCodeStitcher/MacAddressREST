6d807b0b6089c767beb6aeddb0114f46b67f9ceb
indeg <indegcrane@tutanota.com>
2023-04-24 21:39:38 +0100

    main.py: formatting & limiting no of args

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ import requests
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-k", "--key", required=True)
-parser.add_argument("-a", "--address", required=True, nargs="+")
+parser.add_argument("-k", "--key", required=True, nargs=1)
+parser.add_argument("-a", "--address", required=True, nargs=1)
 args = parser.parse_args()
+
 
 def input_checker(arguments):
     # make condition to check if input is a valid MAC address
     # return True if valid, False if invalid
     pass
 
+
 def query_api(arguments):
     # make query to the api here
     pass
 
+
 def run(arguments):
     # call the other functions in here based on some conditions
+    # still need to decide on a useful format for the output
     pass
+
 
 run(args)
